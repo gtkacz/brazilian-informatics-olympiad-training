6026d066f21df196a8230335db74efab99ed8c9a
Gabriel Tkacz <gabriel.tkacz@gscap.com.br>
2023-05-16 21:59:50 -0300

    First two exercises

---
+++ 2022/hotel.py
@@ -0,0 +1,40 @@
+def solve(d: int, a: int, n: int) -> int:
+    days = n
+
+    if n > 15:
+        days = n
+        n = 15
+
+    if n == 1:
+        return 31*d
+
+    else:
+        return (32 - days) * (d + (a * (n-1)))
+
+
+def main():
+    d = int(input())
+    a = int(input())
+    n = int(input())
+
+    return solve(d, a, n)
+
+
+def tests():
+    tests = (
+        (100, 10, 1, 3100),
+        (100, 20, 15, 6460),
+        (100, 5, 16, 2720),
+    )
+
+    for test in tests:
+        d, a, n, expected = test
+        result = solve(d, a, n)
+        assert result == expected
+
+    print('All tests passed successfully!')
+
+
+if __name__ == '__main__':
+    print(main())
+    # tests()
---
+++ 2022/maior.py
@@ -0,0 +1,35 @@
+def solve(n: int, m: int, s: int) -> int:
+    r = tuple(range(m, n, -1))
+
+    for i in r:
+        if sum(tuple(map(int, tuple(str(i))))) == s:
+            return i
+        
+    return -1
+
+
+def main():
+    n = int(input())
+    m = int(input())
+    s = int(input())
+
+    return solve(n, m, s)
+
+
+def tests():
+    tests = (
+        (1, 100, 6, 60),
+        (1000, 1001, 3, -1),
+        (80, 500, 12, 480),
+    )
+
+    for test in tests:
+        n, m, s, expected = test
+        result = solve(n, m, s)
+        assert result == expected
+
+    print('All tests passed successfully!')
+
+if __name__ == '__main__':
+    print(main())
+    # tests()
